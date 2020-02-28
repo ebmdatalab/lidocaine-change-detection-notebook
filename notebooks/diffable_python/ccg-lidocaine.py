522b6fa5c9334a403fcce73e94e32513a6561e4c
Brian <brianmkcebm@gmail.com>
2020-02-28 13:58:19 +0000

    update requirements file

---
+++ notebooks/diffable_python/ccg-lidocaine.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# NBVAL_IGNORE_OUTPUT
+# ^this is a magic comment to work around this issue https://github.com/ebmdatalab/custom-docker/issues/10
+from change_detection import functions as chg
+from lib.outliers import *  #This is copied into the local folder from a branch ebmdatalab pandas library - it will be placed in its own repo to install at a later dat
+
+
