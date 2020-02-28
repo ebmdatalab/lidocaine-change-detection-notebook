420176dc75c1e19c1aae4b81a068bd29680feba8
Brian <brianmkcebm@gmail.com>
2020-02-28 14:26:06 +0000

    update

--- notebooks/diffable_python/ccg-lidocaine.py
+++ notebooks/diffable_python/ccg-lidocaine.py
@@ -19,4 +19,22 @@
 from change_detection import functions as chg
 from lib.outliers import *  #This is copied into the local folder from a branch ebmdatalab pandas library - it will be placed in its own repo to install at a later dat
 
+# NBVAL_IGNORE_OUTPUT
+# ^this is a magic comment to work around this issue https://github.com/ebmdatalab/custom-docker/issues/10
+lidocaine_class = chg.ChangeDetection('ccg_data_lplido%',
+                                    measure=True,
+                                    direction='down',
+                                    use_cache=True,
+                                    overwrite=False,
+                                    verbose=False,
+                                    draw_figures='no')
+lidocaine_class.run()
+
+lidocaine = lidocaine_class.concatenate_outputs()
+lidocaine.head()
+
+filtered_sparkline(lidocaine,
+                  'ccg_data_lplido/ccg_data_lplido',
+                  'ccg_data_lplidocaine')
+
 
